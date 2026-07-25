fafd0bdd91a2644ba79ddcca804cf0083fb9459d
Dan Lewis <dan.lewis@boeing.com>
2026-07-25 13:52:39 -0500

    fix direct call of swing component off EDT, replace with operator

--- src/userInterfaceTest/java/org/netbeans/jemmy/testing/ButtonGridLookupTest.java
+++ src/userInterfaceTest/java/org/netbeans/jemmy/testing/ButtonGridLookupTest.java
@@ -16,15 +16,6 @@
  */
 package org.netbeans.jemmy.testing;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.netbeans.jemmy.util.StringComparators.strict;
-import static org.netbeans.jemmy.util.StringComparators.substring;
-
-import javax.swing.*;
-import java.awt.*;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,8 +26,26 @@ import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JFrameOperator;
 import org.netbeans.jemmy.operators.JLabelOperator;
 import org.netbeans.jemmy.operators.JProgressBarOperator;
+import org.netbeans.jemmy.operators.JToolTipOperator;
 import org.netbeans.jemmy.predicates.AbstractButtonByTextPredicate;
-import org.netbeans.jemmy.util.StringComparators;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JToolTip;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.netbeans.jemmy.util.StringComparators.strict;
+import static org.netbeans.jemmy.util.StringComparators.substring;
 
 // formerly scenario test jemmy_003
 @ExtendWith(DumpOnFailure.class)
@@ -44,7 +53,8 @@ import org.netbeans.jemmy.util.StringComparators;
 @ExtendWith(FastToolTips.class)
 @Timeout(value=60, unit=TimeUnit.SECONDS)
 class ButtonGridLookupTest {
-
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 4;
     private JFrame jFrame;
 
     @BeforeEach
@@ -58,14 +68,14 @@ class ButtonGridLookupTest {
             contentPane.setLayout(new BorderLayout());
             JLabel statusLabel = new JLabel("Button has not been pushed yet");
             contentPane.add(statusLabel, BorderLayout.NORTH);
-            JProgressBar progress = new JProgressBar(0, 4 * 4);
+            JProgressBar progress = new JProgressBar(0, NUM_ROWS * NUM_COLS);
             contentPane.add(progress, BorderLayout.SOUTH);
             JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout(4, 4));
+            panel.setLayout(new GridLayout(NUM_ROWS, NUM_COLS));
             contentPane.add(panel, BorderLayout.CENTER);
             JButton butt;
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < NUM_ROWS; i++) {
+                for (int j = 0; j < NUM_COLS; j++) {
                     butt = new JButton(i + "-" + j);
                     butt.setToolTipText(butt.getText() + " button");
                     butt.addActionListener(event -> {
@@ -96,33 +106,32 @@ class ButtonGridLookupTest {
 
     @Test
     void doit() {
-        JFrameOperator jFrameOp = JFrameOperator.waitFor("ButtonGridLookupTest");
-        JLabelOperator jLabelOp =
-                JLabelOperator.waitFor(jFrameOp, "Button has not been pushed yet", strict());
-        JProgressBarOperator progress = JProgressBarOperator.waitFor(jFrameOp);
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                String bText = i + "-" + j;
-                JButtonOperator jButtonOp = JButtonOperator.waitFor(
-                        jFrameOp, new AbstractButtonByTextPredicate(bText, substring()));
-                assertThat(jButtonOp.getSource())
-                        .isSameAs(AbstractButtonOperator.waitFor(jFrameOp, i * 4 + j)
-                                .getSource());
-                assertThat(jButtonOp.getSource())
-                        .isSameAs(JButtonOperator.waitFor(jFrameOp, i * 4 + j).getSource());
-                assertThat(jButtonOp.showToolTip().getTipText()).isEqualTo(bText + " button");
-                jButtonOp.push();
-                jLabelOp.waitText("Button \"" + bText + "\" has been pushed", strict());
-                progress.waitValue(bText, strict());
-                progress.waitValue(i * 4 + j + 1);
+        JFrameOperator frameOp = JFrameOperator.waitFor("ButtonGridLookupTest");
+        JLabelOperator statusLabelOp = JLabelOperator.waitFor(frameOp, "Button has not been pushed yet", strict());
+        JProgressBarOperator progressBarOp = JProgressBarOperator.waitFor(frameOp);
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                String buttonText = i + "-" + j;
+                JButtonOperator byTextButtonOp = JButtonOperator.waitFor(frameOp, new AbstractButtonByTextPredicate(buttonText, substring()));
+                int buttonIndex = i * NUM_ROWS + j;
+                AbstractButtonOperator byIndexButtonOp = AbstractButtonOperator.waitFor(frameOp, buttonIndex);
+                assertThat(byTextButtonOp.getSource()).isSameAs(byIndexButtonOp.getSource());
+                JToolTip buttonToolTip = byTextButtonOp.showToolTip();
+                JToolTipOperator buttonToolTipOp = JToolTipOperator.of(buttonToolTip);
+                assertThat(buttonToolTipOp.getTipText()).isEqualTo(buttonText + " button");
+                byTextButtonOp.push();
+                statusLabelOp.waitText("Button \"" + buttonText + "\" has been pushed", strict());
+                progressBarOp.waitValue(buttonText, strict());
+                progressBarOp.waitValue(buttonIndex++);
             }
         }
 
-        JButtonOperator buttonOp = JButtonOperator.waitFor(jFrameOp, "0-0", strict());
-        buttonOp.getAccessibleContext().setAccessibleDescription("A button to check different finding approaches");
-        buttonOp.setText("New Text");
-        buttonOp.waitText("New Text", strict());
-        assertThat(jFrameOp.findSubComponent(new AbstractButtonByTextPredicate("New Text", strict())))
-                .isSameAs(buttonOp.getSource());
+        JButtonOperator firstButtonOp = JButtonOperator.waitFor(frameOp, "0-0", strict());
+        firstButtonOp.getAccessibleContext().setAccessibleDescription("A button to check different finding approaches");
+        firstButtonOp.setText("New Text");
+        firstButtonOp.waitText("New Text", strict());
+        Component buttonByTextComponent = frameOp.findSubComponent(new AbstractButtonByTextPredicate("New Text", strict()));
+        assertThat(buttonByTextComponent).isNotNull();
+        assertThat(buttonByTextComponent).isSameAs(firstButtonOp.getSource());
     }
 }
